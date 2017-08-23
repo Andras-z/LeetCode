e5fc34f340dbfd594830fc07b8463dedb1df3c3f
Unknown <564268228@qq.com>
2017-08-23 15:02:29 +0800

    8-23

--- Array/3Sum.py
+++ Array/3Sum.py
@@ -14,19 +14,33 @@ A solution set is:
 nums = [-1, 0, 1, 2, -1, -4]
 nums.sort()
 n = len(nums)
-#[-4, -1, -1, 0, 1, 2]
-
-#NegetiveNums = [x for x in nums if x < 0]
 f = 0
 for i in range(n):
-	if nums[i] >= 0:
+	if nums[i] == 0:
+		f = i + 1
+		break
+	if nums[i] > 0:
 		f = i
 		break
 res = []
 for i in range(f):
-	for j in range(i+1,n-1):
-		for k in range(j+1,n):
-			if nums[i] + nums[j] + nums[k] == 0:
-				res.append([nums[i], nums[j], nums[k]])
-res = list(set(res))
+	if i > 0 and nums[i] == nums[i-1]:
+		continue
+	l = i + 1
+	r = n - 1
+	while(l < r):
+		s = nums[i] + nums[l] + nums[r]
+		if s < 0:
+			l += 1
+		elif s > 0:
+			r -= 1
+		else:
+			res.append([nums[i], nums[l], nums[r]])
+			while l < r and nums[l] == nums[l+1]:
+				l += 1
+			while l < r and nums[r] == nums[r-1]:
+				r -= 1
+			l += 1
+			r -= 1
+
 print (res)
