2fd92ecd9615c4f406db72fc4551abce493e071d
Unknown <564268228@qq.com>
2017-08-23 17:25:46 +0800

    改进的二分查找

--- Array/Search in Rotated Sorted Array.py
+++ Array/Search in Rotated Sorted Array.py
@@ -50,3 +50,28 @@ else:
 			else:
 				l = m + 1
 return -1
+
+
+#Discuss
+if not nums:
+    return -1
+
+low, high = 0, len(nums) - 1
+
+while low <= high:
+    mid = (low + high) / 2
+    if target == nums[mid]:
+        return mid
+
+    if nums[low] <= nums[mid]:
+        if nums[low] <= target <= nums[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+    else:
+        if nums[mid] <= target <= nums[high]:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+return -1
